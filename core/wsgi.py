30f9ec385813e0c05009b196f89c55ec350450cf
yahya <yahyadkiouak1@gmail.com>
2025-11-16 13:13:19 +0100

    Fix Django settings module for Railway

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = get_wsgi_application()
+
